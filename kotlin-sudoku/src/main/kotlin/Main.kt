73626d60ce6d36ecf0894c96411e1fbcbe4291c3
cdpjenkins <cdpjenkins@gmail.com>
2023-07-30 19:34:44 +0100

    Start parsing a board file

--- kotlin-sudoku/src/main/kotlin/Main.kt
+++ kotlin-sudoku/src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val contents = File("board1.txt").readLines()
+
+    println(contents)
+
+    val cells =
+        contents.withIndex().flatMap { (y, line) ->
+            line.withIndex().map { (x, ch) ->
+                Position(x, y) to Cell.parse(ch)
+            }
+        }.toMap()
+
+    println(cells   )
 }
+
+data class Cell(val possibilities: Set<Int>) {
+    companion object {
+        fun parse(ch: Char) = Cell(if (ch.isDigit()) setOf(ch.code) else emptySet())
+    }
+}
+
+data class Position(val x: Int, val y:Int)
+
