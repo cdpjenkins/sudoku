93b62ea15d139974d96d1b7936415f6884b21345
Chris Jenkins <cdpjenkins@gmail.com>
2023-07-30 21:04:31 +0100

    Can set a cell
    No constraint propagation happens yet.

--- kotlin-sudoku/src/main/kotlin/Board.kt
+++ kotlin-sudoku/src/main/kotlin/Board.kt
@@ -1,14 +1,15 @@
-data class Board(val cells: Map<Position, Cell>) {
+data class Board(val cells: MutableMap<Position, Cell>) {
     fun dump() {
         println(dumpToString())
     }
 
-    fun dumpToString() =
-        (0..8).map { y: Int ->
+    fun dumpToString(): String {
+        return (0..8).map { y: Int ->
             (0..8).map { x: Int ->
                 cells[Position(x, y)]!!.dumpToString()
             }.joinToString("")
         }.joinToString("\n")
+    }
 
     companion object {
         fun parse(contents: String): Board {
@@ -18,9 +19,36 @@ data class Board(val cells: Map<Position, Cell>) {
                         line.withIndex().map { (x, ch) ->
                             Position(x, y) to Cell.parse(ch)
                         }
-                    }.toMap()
+                    }.toMap().toMutableMap()
             return Board(cells)
         }
+
+        fun populate(contents: String): Board {
+            val board =
+                Board(
+                    (0..8).flatMap { y ->
+                        (0..8).map { x ->
+                            Position(x, y) to Cell(emptySet())
+                        }
+                    }.toMap().toMutableMap()
+                )
+
+            contents.lines()
+                .withIndex().flatMap { (y, line) ->
+                    line.withIndex().map { (x, ch) ->
+                        val value = ch.digitToIntOrNull()
+
+                        if (value != null) {
+                            board.setCell(Position(x, y), value)
+                        }
+                    }
+                }
+            return board
+        }
+    }
+
+    fun setCell(position: Position, value: Int) {
+        cells[position] = Cell(setOf(value))
     }
 }
 
@@ -36,8 +64,6 @@ data class Cell(val possibilities: Set<Int>) {
         fun parse(ch: Char) = Cell(ch.digitToIntOrNull()?.let { setOf(it) } ?: emptySet())
     }
 }
-
-
 
 data class Position(val x: Int, val y:Int) {
 
--- kotlin-sudoku/src/test/kotlin/BoardTest.kt
+++ kotlin-sudoku/src/test/kotlin/BoardTest.kt
@@ -4,13 +4,47 @@ import org.junit.jupiter.api.Test
 class BoardTest {
     @Test
     fun `can parse and print a board`() {
-        val board = Board.parse(BOARD1)
+        val board = Board.populate(BOARD1_INPUT)
 
-        board.dumpToString() shouldBe BOARD1
+        board.dumpToString() shouldBe BOARD1_INPUT
+    }
+
+    @Test
+    fun `can set a single cell`() {
+        val board = Board.populate(EMPTY_BOARD)
+
+        board.setCell(Position(1, 1), 7)
+
+        board.dumpToString() shouldBe EMPTY_BOARD_WITH_7_ADDED
     }
 }
 
-val BOARD1 = """
+val EMPTY_BOARD = """
+            _________
+            _________
+            _________
+            _________
+            _________
+            _________
+            _________
+            _________
+            _________
+        """.trimIndent()
+
+val EMPTY_BOARD_WITH_7_ADDED = """
+            _________
+            _7_______
+            _________
+            _________
+            _________
+            _________
+            _________
+            _________
+            _________
+        """.trimIndent()
+
+
+val BOARD1_INPUT = """
             53__7____
             6__195___
             _98____6_
