f5985f6f794e399cbf74481758f1abdb66818e67
Chris Jenkins <cdpjenkins@gmail.com>
2023-08-02 20:58:46 +0100

    Print time to solve

--- kotlin-sudoku/src/main/kotlin/Main.kt
+++ kotlin-sudoku/src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 fun main() {
     val contents = File("board2.txt").readText()
 
-    val board = Board.populate(contents).solve()
+    val timeInMillis = measureTimeMillis {
+        val board = Board.populate(contents).solve()
+        board.dump()
+    }
 
-    board.dump()
+    println()
+    println("${timeInMillis}ms")
 }
